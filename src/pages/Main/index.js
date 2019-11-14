f0397bb6a67874bbbce0eb510d3fb0c64eef1391
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-14 18:51:59 -0300

    Estilizando a pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './style';
+// import { Title } from './style';
+import { Container, Form, SubmitButton } from './style';
 
 function Main() {
-  return <Title>Hello World!</Title>;
+  return (
+    // <Title>Hello World!</Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton disabled>
+          {' '}
+          <FaPlus color="#FFF" size={14} />{' '}
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
